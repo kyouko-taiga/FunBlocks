6bae3450f7c5064eec6481d156f7d29fa6fdb1f1
Dimitri Racordon <dimitri.racordon@gmail.com>
2020-02-11 17:18:48 +0100

    Add styling to the whole page

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,8 @@ import React from 'react'
 import { render } from 'react-dom'
 import { Provider } from 'react-redux'
 
+import './Styles/Foundation.css'
+
 // This import configures fontawesome.
 import './Icons'
 
