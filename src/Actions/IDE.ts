eaee72eb3da2084731dcb92b9b9aeb675fa4b834
Dimitri Racordon <dimitri.racordon@gmail.com>
2020-02-11 22:59:44 +0100

    Merge all IDE actions in a single module

--- src/Actions/IDE.ts
+++ src/Actions/IDE.ts
@@ -1,28 +1,50 @@
 import { PayloadAction } from 'FunBlocks/Actions/Types'
-import { Term } from 'FunBlocks/AST/Terms'
-import { IDEMode } from 'FunBlocks/Reducers/IDE'
+import { Term, Rule } from 'FunBlocks/AST/Terms'
+import { IDEMode } from 'FunBlocks/Reducers'
 
-export const PUSH_STATE = 'IDE.pushState'
-export const SELECT_RULE = 'IDE.selectRule'
-export const SET_HISTORY_INDEX = 'IDE.setHistoryIndex'
-export const SET_MODE = 'IDE.setMode'
+export const ACTION_TYPES = {
+  SET_MODE              : 'IDE.setMode',
+
+  INSERT_RULE           : 'IDE.editMode.insertRule',
+  UPDATE_INITIAL_STATE  : 'IDE.editMode.updateInitialState',
+
+  PUSH_STATE            : 'IDE.debugMode.pushState',
+  SELECT_RULE           : 'IDE.debugMode.selectRule',
+  SET_HISTORY_INDEX     : 'IDE.debugMode.setHistoryIndex',
+}
+
+// ----- General actions --------------------------------------------------------------------------
 
 export const pushState = (state: Term): PayloadAction<Term> => ({
-  type: PUSH_STATE,
+  type: ACTION_TYPES.PUSH_STATE,
   payload: state,
 })
 
+// ----- Edit mode actions -----------------------------------------------------------------------
+
+export const insertRule = (rule: Rule): PayloadAction<Rule> => ({
+  type: ACTION_TYPES.INSERT_RULE,
+  payload: rule,
+})
+
+export const updateInitialState = (state: Term): PayloadAction<Term> => ({
+  type: ACTION_TYPES.UPDATE_INITIAL_STATE,
+  payload: state,
+})
+
+// ----- Debug mode actions -----------------------------------------------------------------------
+
 export const selectRule = (ruleID: string): PayloadAction<string> => ({
-  type: SELECT_RULE,
+  type: ACTION_TYPES.SELECT_RULE,
   payload: ruleID,
 })
 
 export const setHistoryIndex = (index: number): PayloadAction<number> => ({
-  type: SET_HISTORY_INDEX,
+  type: ACTION_TYPES.SET_HISTORY_INDEX,
   payload: index,
 })
 
 export const setMode = (mode: IDEMode): PayloadAction<IDEMode> => ({
-  type: SET_MODE,
+  type: ACTION_TYPES.SET_MODE,
   payload: mode,
 })
--- src/Actions/Program.ts
+++
@@ -1,9 +0,0 @@
-import { PayloadAction } from 'FunBlocks/Actions/Types'
-import { Rule } from 'FunBlocks/AST/Terms'
-
-export const INSERT_RULE = 'Program.insertRule'
-
-export const insertRule = (rule: Rule): PayloadAction<Rule> => ({
-  type: INSERT_RULE,
-  payload: rule,
-})
