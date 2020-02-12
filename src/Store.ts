c3bdc2c38e8943b4fa17c9f815afac31fd011a2e
Dimitri Racordon <dimitri.racordon@gmail.com>
2020-02-13 00:31:23 +0100

    Implement a throttling middleware for redux

--- src/Store.ts
+++ src/Store.ts
@@ -1,11 +1,15 @@
-import { createStore, applyMiddleware } from 'redux'
+import { applyMiddleware, createStore } from 'redux'
 import { createLogger } from 'redux-logger'
 
 import reducers from './Reducers'
+import { Program as _Program } from './Reducers/Program'
+import throttler from './Utils/ReduxThrottler'
 
 const mware = applyMiddleware(
+  throttler,
   createLogger({ collapsed: true, diff: false }))
 
 export type RootState = ReturnType<typeof reducers>
+export type Program = _Program
 
 export const store = createStore(reducers, mware)
---
+++ src/Utils/ReduxThrottler.ts
@@ -0,0 +1,23 @@
+import { Dispatch, Middleware, MiddlewareAPI } from 'redux'
+
+const throttled: { [key: string]: boolean } = {}
+
+const throttler: Middleware = ({ getState }: MiddlewareAPI) => (next: Dispatch) => (action) => {
+  const delay = action.meta && action.meta.throttlingDelay
+  if (typeof delay !== 'number' || delay <= 0) {
+    return next(action)
+  }
+
+  if (throttled[action.type]) {
+    return
+  }
+
+  throttled[action.type] = true
+  setTimeout(() => {
+    throttled[action.type] = false
+  }, delay);
+
+  return next(action)
+}
+
+export default throttler
