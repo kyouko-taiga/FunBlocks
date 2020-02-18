2e8eebf3860384fca2e96ad7a21d6d7e9324263d
Dimitri Racordon <dimitri.racordon@gmail.com>
2020-02-18 23:42:27 +0100

    Implement a download button

--- src/Components/IDE/MenuBar.tsx
+++ src/Components/IDE/MenuBar.tsx
@@ -5,6 +5,7 @@ import { Dispatch } from 'redux'
 import { setMode } from 'FunBlocks/Actions/IDE'
 import { IDEMode } from 'FunBlocks/Reducers'
 import { RootState } from 'FunBlocks/Store'
+import DownloadButton from './DownloadButton'
 import MenuBarItem from './MenuBarItem'
 
 const styles = require('./IDE.module')
@@ -20,24 +21,29 @@ class MenuBar extends React.PureComponent<Props> {
     const mode = this.props.mode
     return (
       <div className={ styles.menuBar }>
-        <MenuBarItem
-          icon="pen"
-          label="Edit"
-          pressed={ mode === IDEMode.Edit }
-          onClick={ () => this.props.setIDEMode(IDEMode.Edit) }
-        />
-        <MenuBarItem
-          icon="bug"
-          label="Debug"
-          pressed={ mode === IDEMode.Debug }
-          onClick={ () => this.props.setIDEMode(IDEMode.Debug) }
-        />
-        <MenuBarItem
-          icon="play"
-          label="Run"
-          pressed={ mode === IDEMode.Run }
-          onClick={ () => this.props.setIDEMode(IDEMode.Run) }
-        />
+        <div className={ styles.menuGroup }>
+          <MenuBarItem
+            icon="pen"
+            label="Edit"
+            pressed={ mode === IDEMode.Edit }
+            onClick={ () => this.props.setIDEMode(IDEMode.Edit) }
+          />
+          <MenuBarItem
+            icon="bug"
+            label="Debug"
+            pressed={ mode === IDEMode.Debug }
+            onClick={ () => this.props.setIDEMode(IDEMode.Debug) }
+          />
+          <MenuBarItem
+            icon="play"
+            label="Run"
+            pressed={ mode === IDEMode.Run }
+            onClick={ () => this.props.setIDEMode(IDEMode.Run) }
+          />
+        </div>
+        <div className={ styles.menuGroup }>
+          <DownloadButton />
+        </div>
       </div>
     )
   }
