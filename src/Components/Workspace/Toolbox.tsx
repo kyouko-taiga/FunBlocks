5a3075243f7dde8ee7a8d0d99a582bffda125215
Dimitri Racordon <dimitri.racordon@gmail.com>
2020-02-18 18:20:43 +0100

    Add a new rule instance to the dragged data

--- src/Components/Workspace/Toolbox.tsx
+++ src/Components/Workspace/Toolbox.tsx
@@ -2,7 +2,7 @@ import classNames from 'classnames'
 import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
-import { Expression, Variable } from 'FunBlocks/AST/Terms'
+import { Expression, Variable, Rule } from 'FunBlocks/AST/Terms'
 import Block from 'FunBlocks/Components/Block'
 import ObjectTrash from './ObjectTrash'
 import ToolButton from './ToolButton'
@@ -20,15 +20,15 @@ class Toolbox extends React.PureComponent {
         <div className={ styles.sectionHeading }>Objects</div>
         <div className={ styles.toolMatrix }>
           <div className={ styles.row }>
-            <ToolButton label="Expression" draggedData={ this.createExprDraggedData.bind(this) }>
+            <ToolButton label="Expression" draggedData={ this.createExprData.bind(this) }>
               <Block term={ this.dummyExpr } />
             </ToolButton>
-            <ToolButton label="Variable" draggedData={ this.createVarDraggedData.bind(this) }>
+            <ToolButton label="Variable" draggedData={ this.createVarData.bind(this) }>
               <Block term={ this.dummyVar } />
             </ToolButton>
           </div>
           <div className={ styles.row }>
-            <ToolButton label="Rule" colspan={ 2 } draggedData={ { type: 'Rule' } }>
+            <ToolButton label="Rule" colspan={ 2 } draggedData={ this.createRuleData.bind(this) }>
               <Block term={ this.dummyExpr } />
               <FontAwesomeIcon icon="arrow-right" size="lg" />
               <Block term={ this.dummyExpr } />
@@ -41,17 +41,24 @@ class Toolbox extends React.PureComponent {
     )
   }
 
-  createExprDraggedData() {
+  createExprData() {
     return {
       type: 'Term',
       payload: new Expression({ label: 'abc' }),
     }
   }
 
-  createVarDraggedData() {
+  createVarData() {
     return {
       type: 'Term',
       payload: new Variable({ label: 'x' }),
+    }
+  }
+
+  createRuleData() {
+    return {
+      type: 'Rule',
+      payload: new Rule({ left: this.dummyExpr, right: this.dummyExpr }),
     }
   }
 
