541a5b70e33a73f063f5b035e6410735392fdd7a
Dimitri Racordon <dimitri.racordon@gmail.com>
2020-02-19 15:39:20 +0100

    Fix an error in the type definitions

--- tools/Parser/Sources/FunBlockParser/main.swift
+++ tools/Parser/Sources/FunBlockParser/main.swift
@@ -12,11 +12,11 @@ rule sort(empty) => empty
 rule sort(cons($x, $y)) => cat(filter_lt($x, $y), cons($x, sort(filter_gt($x, $y))))
 
 // Filters
-decl filter_lt :: List Int => List Int
+decl filter_lt :: Int -> List Int => List Int
 rule filter_lt($x, empty) => empty
 rule filter_lt($x, cons($y, $z)) => if(lt($y, $x), cons($y, filter_lt($z)), filter_lt($z))
 
-decl filter_gt :: List Int => List Int
+decl filter_gt :: Int -> List Int => List Int
 rule filter_gt($x, empty) => empty
 rule filter_gt($x, cons($y, $z)) => if(gt($y, $x), cons($y, filter_gt($z)), filter_gt($z))
 
