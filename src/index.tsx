578ec699115d901491d50a00586d3b97a0ed1996
Dimitri Racordon <dimitri.racordon@gmail.com>
2020-02-19 16:36:39 +0100

    Remove debug code

--- src/index.tsx
+++ src/index.tsx
@@ -7,87 +7,8 @@ import './Styles/Foundation.css'
 // This import configures fontawesome.
 import './Icons'
 
-// // This imports the styles for react-grid-layout.
-// import 'react-grid-layout/css/styles.css'
-// import 'react-resizable/css/styles.css'
-// import GridLayout from 'react-grid-layout'
-
-import { Expression, Variable, Rule } from './AST/Terms'
-import { EnumType } from './AST/Types'
 import IDE from './Components/IDE'
-import Color from './Utils/Color'
 import { store } from './Store'
-
-import { insertRule, updateInitialState } from './Actions/IDE'
-
-const ty = new EnumType({
-  name: 'Ty', cases: [ new Expression({ label: 'unit' }), new Expression({ label: 'bar' }) ]
-})
-const foo = (): Expression => new Expression({ label: 'foo', type: ty })
-const bar = (): Expression => new Expression({ label: 'bar', type: ty })
-
-const empty = (): Expression => new Expression({ label: 'empty' })
-const list = (terms: Array<Term>): Expression => {
-  if (terms.length == 0) {
-    return empty()
-  } else {
-    return new Expression({
-      label: 'cons',
-      subterms: [ terms[0], list(terms.slice(1)) ]
-    })
-  }
-}
-
-const v = (label: string): Variable => new Variable({ label, type: ty })
-
-store.dispatch(updateInitialState(list([foo(), foo(), bar()])))
-store.dispatch(insertRule(
-  new Rule({
-    left: list([foo(), v('x')]),
-    right: list([v('x')]),
-  })))
-
-// const planetType = new EnumType({
-//   name: 'Planet',
-//   cases: [
-//     new Expression({ label: 'earth' }),
-//     new Expression({ label: 'mars' })
-//   ],
-// })
-//
-// const planetListType = new EnumType({ name: 'List' })
-// planetListType.cases = [
-//   new Expression({ label: 'empty' }),
-//   new Expression({
-//     label: 'cons',
-//     subterms: [
-//       new Variable({ label: 'head', type: planetType }),
-//       new Variable({ label: 'tail', type: planetListType }),
-//     ]
-//   })
-// ]
-//
-// const expr = new Expression({
-//   label: 'cons',
-//   subterms: [
-//     new Expression({ label: 'earth', type: planetType }),
-//     new Expression({
-//       label: 'cons',
-//       subterms: [
-//         new Variable({ label: 'x', type: planetType }),
-//         new Expression({ label: 'empty', type: planetListType }),
-//       ],
-//       type: planetListType,
-//     }),
-//   ],
-//   type: planetListType,
-// })
-//
-// store.dispatch(pushState(expr))
-// store.dispatch(insertRule(new Rule({
-//   left: expr.subterms[1],
-//   right: (expr.subterms[1] as Expression).subterms[1],
-// })))
 
 class View extends React.PureComponent {
 
