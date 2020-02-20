ca1e653e14efc7dfa04e54fe8292361e6f37bff8
Dimitri Racordon <dimitri.racordon@gmail.com>
2020-02-20 18:13:49 +0100

    Coding style

--- src/Parser/Parser.ts
+++ src/Parser/Parser.ts
@@ -1,258 +1,200 @@
-import { string, whitespace, regexp, newline, eof, anyChar, fail } from "parjs"
+import { anyChar, eof, fail, newline, regexp, string, whitespace } from 'parjs'
 import {
   between,
-  manySepBy,
-  thenq,
   later,
   many,
+  manySepBy,
+  manyTill,
   map,
+  maybe,
+  or,
+  qthen,
+  recover,
   stringify,
   then,
-  qthen,
-  or,
-  maybe,
-  manyTill,
-  recover
-} from "parjs/combinators"
+  thenq,
+} from 'parjs/combinators'
 
 import * as AST from 'FunBlocks/AST'
 
-let alphanumeric = regexp(new RegExp("[a-zA-Z0-9_]"));
+const alphanumeric = regexp(new RegExp("[a-zA-Z0-9_]"))
 
-let terminator = newline().pipe(
+const terminator = newline().pipe(
   or(string(";")),
   or(eof()),
-  between(whitespace())
-);
-// console.log(terminator.parse(" ; "))
+  between(whitespace()))
 
-let alphaSequence = alphanumeric.pipe(
+const alphaSequence = alphanumeric.pipe(
   then(alphanumeric.pipe(many())),
   stringify(),
-  thenq(whitespace().pipe(maybe()))
-);
+  thenq(whitespace().pipe(maybe())))
 
-let identifier = alphaSequence;
+const identifier = alphaSequence
 
-let variable = string("$").pipe(
+const variable = string("$").pipe(
   qthen(identifier),
-  map(str => new AST.VarRef({ label: str }))
-);
+  map((str) => new AST.VarRef({ label: str })))
 
-let typeVarRef = variable.pipe(
-  map(e => new AST.TypeVarRef({ label: e.label }))
-);
+const typeVarRef = variable.pipe(
+  map((e) => new AST.TypeVarRef({ label: e.label })))
 
-let typeParamList = typeVarRef.pipe(manySepBy(whitespace()));
+const typeParamList = typeVarRef.pipe(manySepBy(whitespace()))
 
-let typeDeclRef = later<AST.TypeDeclRef>();
-let typeArg = later<AST.TypeRef>();
-let typeArgList = later<AST.TypeRef[]>();
+const typeDeclRef = later<AST.TypeDeclRef>()
 
-let typeRef = typeDeclRef.pipe(
-  or(typeVarRef)
-  , map(e => e as AST.TypeRef )
-);
+const typeArg = later<AST.TypeRef>()
 
-typeArg.init(
-  typeRef.pipe(
-    between("(", ")")
-    ,or(typeRef)
-  )
-);
+const typeArgList = later<AST.TypeRef[]>()
 
+const typeRef = typeDeclRef.pipe(
+  or(typeVarRef),
+  map((e) => e as AST.TypeRef))
 
-typeArgList.init(
-  typeArg.pipe(
-    then(typeArg.pipe(
-      manySepBy(whitespace())
-    ))
-    , map(arr => new Array(arr[0]).concat(arr[1]) )
-  )
-);
+typeArg.init(typeRef.pipe(
+  between("(", ")"),
+  or(typeRef)))
 
-let typeCons =  identifier.pipe(
-    then(
-      typeArgList.pipe(
-        between(whitespace()),
-        maybe()
-      )
-    ),
+typeArgList.init(typeArg.pipe(
+  then(typeArg.pipe(manySepBy(whitespace()))),
+  map((arr) => new Array(arr[0]).concat(arr[1]))))
+
+const typeCons = identifier.pipe(
+  then(
+    typeArgList.pipe(
+      between(whitespace()),
+      maybe())),
     thenq(whitespace()),
-    // create a typeArg[]
-    map(arr => new AST.TypeConsDecl({ label: arr[0], arguments: arr[1] }))
-  );
+    map((arr) => new AST.TypeConsDecl({ label: arr[0], arguments: arr[1] })))
 
-let term = later<Term>();
+const term = later<Term>()
 
-let termList = term.pipe(
+const termList = term.pipe(
   between(whitespace()),
   manySepBy(","),
-  map(arr => arr as Term[])
-);
+  map((arr) => arr as Term[]))
 
-let expression = identifier.pipe(
-  then(
-    termList.pipe(
-      between("(", ")"),
-      maybe()
-    )
-  ),
+const expression = identifier.pipe(
+  then(termList.pipe(
+    between("(", ")"),
+    maybe())),
   thenq(whitespace()),
-  map(arr => new AST.Expr({ label: arr[0], subterms: arr[1] }))
-);
+  map((arr) => new AST.Expr({ label: arr[0], subterms: arr[1] })))
 
-term.init(
-  expression.pipe(
-    or(variable),
-    map(term => term as Term)
-  )
-);
+term.init(expression.pipe(
+  or(variable),
+  map(term => term as Term)))
 
-let initStateDecl = string("init").pipe(
+const initStateDecl = string("init").pipe(
   qthen(expression.pipe(between(whitespace()))),
   thenq(terminator),
-  map(expr => new AST.InitStateDecl({ state: expr }))
-);
+  map((expr) => new AST.InitStateDecl({ state: expr })))
 
-let parenthExpression = expression.pipe(
-  between(whitespace())
-  , between("(",")")
-);
+const parenthExpression = expression.pipe(
+  between(whitespace()),
+  between("(",")"))
 
-let parenthTerm = term.pipe(
-  between(whitespace())
-  , between("(",")")
-);
+const parenthTerm = term.pipe(
+  between(whitespace()),
+  between("(",")"))
 
-let ruleCaseDecl = string("rule").pipe(
-  qthen(expression.pipe(or(parenthExpression), between(whitespace())))
-  , then(
-    string("=>").pipe(
-      between(whitespace()),
-      qthen(term.pipe(or(parenthTerm)))
-    )
-  )
-  , thenq(whitespace())
-  , thenq(terminator)
-  , map(arr => new AST.RuleCaseDecl({ left: arr[0], right: arr[1] }))
-);
+const ruleCaseDecl = string("rule").pipe(
+  qthen(expression.pipe(
+    or(parenthExpression), between(whitespace()))),
+  then(string("=>").pipe(
+    between(whitespace()),
+    qthen(term.pipe(
+      or(parenthTerm))))),
+  thenq(whitespace()),
+  thenq(terminator),
+  map((arr) => new AST.RuleCaseDecl({ left: arr[0], right: arr[1] })))
 
+const typeSign = later()
 
-let typeSign = later();
+const parenthTypeSign = typeSign.pipe(
+  between(whitespace()),
+  between("(",")"))
 
-let parenthTypeSign = typeSign.pipe(between(whitespace()), between("(",")"));
+const enumTypeTail = string("|").pipe(
+  qthen(typeCons.pipe(
+    between(whitespace()),
+    manySepBy("|"))))
 
+const enumTypeBody = typeCons.pipe(
+  then(enumTypeTail.pipe(maybe())),
+  between(whitespace()),
+  map((arr) => new Array(arr[0]).concat(arr[1])))
 
-let enumTypeTail = string("|").pipe(
-  qthen(typeCons.pipe(between(whitespace()), manySepBy("|")))
-);
+typeDeclRef.init(identifier.pipe(
+  then(typeArgList.pipe(
+    between(whitespace()),
+    maybe())),
+  thenq(whitespace()),
+  map((arr) => new AST.TypeDeclRef({ name: arr[0], arguments: arr[1] }))))
 
+const right = typeSign.pipe(
+  or(parenthTypeSign),
+  map((e) => e as AST.TypeSign))
 
-let enumTypeBody = typeCons.pipe(
-  then(enumTypeTail.pipe(maybe()))
-  , between(whitespace())
-  , map( arr =>  new Array(arr[0]).concat(arr[1]) )
-);
+const left = typeRef.pipe(
+  or(parenthTypeSign),
+  map((e) => e as AST.TypeSign))
 
-
-typeDeclRef.init(
-  identifier.pipe(
-    then(
-      typeArgList.pipe(
-        between(whitespace()),
-        maybe()
-      )
-    ),
-    thenq(whitespace()),
-    map(arr => new AST.TypeDeclRef({ name: arr[0], arguments: arr[1] }))
-  )
-
-);
-
-
-let right = typeSign.pipe(or(parenthTypeSign), map(e => e as AST.TypeSign))
-
-let left = typeRef.pipe(or(parenthTypeSign), map(e => e as AST.TypeSign))
-
- let arrowTypeSign = left.pipe(
+const arrowTypeSign = left.pipe(
   then(string("->").pipe(
-    between(whitespace())
-    ,recover(
-      failure => { return {kind: "Soft", reason: "custom soft failure"} }
-    )
-    , qthen(right)
-    , thenq(whitespace())
-  ))
-  , map(
-    arr => new AST.ArrowTypeSign({ left: arr[0], right: arr[1] })
-  )
-);
+    between(whitespace()),
+    recover((failure) => ({ kind: "Soft", reason: "custom soft failure" })),
+    qthen(right),
+    thenq(whitespace()))),
+  map((arr) => new AST.ArrowTypeSign({ left: arr[0], right: arr[1] })))
 
-typeSign.init(
-  arrowTypeSign.pipe(
-    recover(
-      failure => {
-        if(failure.kind === "Hard" && failure.reason === 'custom soft failure'){
-          return {kind: "Soft", reason: "custom soft failure 2"}
-        }
-      }
-    )
-    , or(typeRef)
-  , map(e=> e as AST.TypeSign))
-);
+typeSign.init(arrowTypeSign.pipe(
+  recover((failure) => {
+    if(failure.kind === "Hard" && failure.reason === 'custom soft failure') {
+      return { kind: "Soft", reason: "custom soft failure 2" }
+    }
+  }),
+  or(typeRef),
+  map((e) => e as AST.TypeSign)))
 
+const ruleDeclBody = typeSign.pipe(
+  or(parenthTypeSign),
+  thenq(string("=>").pipe(
+    between(whitespace()))),
+  then(typeSign),
+  map((arr) => arr as Array<AST.TypeSign>))
 
-let ruleDeclBody = typeSign.pipe(
-  or(parenthTypeSign)
-  , thenq(string("=>").pipe(
-    between(whitespace())
-    )
-  )
-  , then(typeSign)
-  , map(
-    arr => arr as AST.TypeSign[] )
-);
+const ruleDecl = string("decl").pipe(
+  qthen(identifier.pipe(between(whitespace()))),
+  then(typeParamList),
+  thenq(string("::").pipe(
+    between(whitespace()))),
+  then(ruleDeclBody.pipe(
+    between(whitespace()))),
+  thenq(terminator),
+  map((arr) => new AST.RuleDecl({
+    name: arr[0][0],
+    arguments: arr[0][1],
+    left: arr[1][0],
+    right: arr[1][1]
+  })))
 
-let ruleDecl = string("decl").pipe(
-  qthen(identifier.pipe(between(whitespace())))
-  , then(typeParamList)
-  , thenq(string("::").pipe(between(whitespace())))
-  , then(ruleDeclBody.pipe(between(whitespace())))
-  , thenq(terminator)
-  , map(
-    arr =>
-      new AST.RuleDecl({
-        name: arr[0][0],
-        arguments: arr[0][1],
-        left: arr[1][0],
-        right: arr[1][1]
-      })
-  )
-);
-
-let typeDecl = string("type").pipe(
+const typeDecl = string("type").pipe(
   qthen(identifier.pipe(between(whitespace()))),
   then(typeParamList.pipe(maybe())),
   thenq(string("::").pipe(between(whitespace()))),
   then(enumTypeBody.pipe(between(whitespace()))),
   thenq(terminator),
-  map(
-    arr => new AST.TypeDecl({ name: arr[0][0], parameters: arr[0][1], cases : arr[1] })
-  )
-);
+  map((arr) => new AST.TypeDecl({ name: arr[0][0], parameters: arr[0][1], cases : arr[1] })))
 
-let topLevelStmt = typeDecl.pipe(
+const topLevelStmt = typeDecl.pipe(
   or(initStateDecl),
   or(ruleDecl),
   or(ruleCaseDecl),
-  between(whitespace())
-);
+  between(whitespace()))
 
+const program = topLevelStmt.pipe(many())
 
-let program = topLevelStmt.pipe(many());
-
-let programInput = `
+const programInput = `
 init cons(1, cons(2, empty)) ;
 
 type List $T :: empty | cons $T (List $T) ;
@@ -273,6 +215,6 @@ decl if $T :: Bool -> $T -> $T => $T ;
 
 rule if(true, $x, $y) => $x ;
 rule if(false, $x, $y) => $y ;
-`;
+`
 
-console.log(" program 1)", program.parse(programInput));
+console.log(' program 1)', program.parse(programInput))
