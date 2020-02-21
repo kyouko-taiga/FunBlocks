f3dd427aa9e8661d15155d5c059dfbfe97c9ce16
Dimitri Racordon <dimitri.racordon@gmail.com>
2020-02-21 16:10:11 +0100

    Integrate the ACE editor

--- src/UI/Components/Workspace/ProgramEditor.tsx
+++ src/UI/Components/Workspace/ProgramEditor.tsx
@@ -1,15 +1,38 @@
+import classNames from 'classnames'
 import React from 'react'
+import AceEditor from "react-ace"
+import { connect } from 'react-redux'
 
+import "ace-builds/webpack-resolver"
+import "ace-builds/src-noconflict/theme-github"
+
+import { InputMode } from 'FunBlocks/UI/Reducers'
+import { RootState } from 'FunBlocks/UI/Store'
 import RulesEditor from './RulesEditor'
 import StateEditor from './StateEditor'
 
 const styles = require('./Workspace.module')
 
-class ProgramEditor extends React.PureComponent {
+type Props = {
+  inputMode: InputMode,
+}
+
+class ProgramEditor extends React.PureComponent<Props> {
 
   render() {
+    switch (this.props.inputMode) {
+    case InputMode.Visual:
+      return this.renderVisual()
+    case InputMode.Textual:
+      return this.renderTextual()
+    default:
+      return null
+    }
+  }
+
+  renderVisual() {
     return (
-      <div className={ styles.programEditor }>
+      <div className={ classNames(styles.programEditor, styles.visual) }>
         <div className={ styles.sectionHeading }>Initial State</div>
         <StateEditor />
         <div className={ styles.sectionHeading }>Rules</div>
@@ -18,6 +41,25 @@ class ProgramEditor extends React.PureComponent {
     )
   }
 
+  renderTextual() {
+    return (
+      <div className={ classNames(styles.programEditor, styles.textual) }>
+        <AceEditor
+          mode="funblocks"
+          theme="github"
+          name="program-editor"
+          width="100%"
+          height="100%"
+          fontSize={ 16 }
+        />
+      </div>
+    )
+  }
+
 }
 
-export default ProgramEditor
+const mapStateToProps = (state: RootState) => ({
+  inputMode: state.inputMode,
+})
+
+export default connect(mapStateToProps)(ProgramEditor)
